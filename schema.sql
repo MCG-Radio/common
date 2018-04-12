c8eef2d03789a64c4483d31701dda3721fe4b1e3
Alexandre Petitjean <alexandre.petitjean@echosens.com>
2018-04-12 10:29:34 +0200

    Add volume

--- schema.sql
+++ schema.sql
@@ -3,7 +3,8 @@ create table if not exists stations (
     name text not null,
     position integer not null unique,
     stream_url text not null,
-    image_url text
+    image_url text,
+    volume integer default 80
 );
 
 create table if not exists current_info (
